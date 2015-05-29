0f76f7fe03be34e4a0765eeb2c1c49c9815e6122
hecferme <hecferme@gmail.com>
2015-05-28 22:02:26 -0600

    versión semana 3 - miércoles de la clase de java

--- Calentamiento/src/Semanas-1-2/EcuacionCuadratica.java
+++ Calentamiento/src/Semanas-1-2/EcuacionCuadratica.java
@@ -11,6 +11,86 @@ package Semana1y2;
  */
 public class EcuacionCuadratica {
     
+    String[] ArrCeroAQuince = {"CERO", "UNO", "DOS", "TRES", "CUATRO", "CINCO",
+                             "SEIS", "SIETE", "OCHO", "NUEVE", "DIEZ", "ONCE",
+                             "DOCE", "TRECE", "CATORCE", "QUINCE"};
+    String [] ArrDiecis = {"NULO", "DIECI", "VEINT", "TREINTA", "CUARENTA", "CINCUENTA",
+                            "SESENTA", "SETENTA", "OCHENTA", "NOVENTA"};
+    String [] ArrCentenas = {"CERO", "CIEN", "DOSCIENTOS", "TRESCIENTOS", 
+                             "CUATROCIENTOS", "QUINIENTOS", "SEISCIENTOS", 
+                             "SETECIENTOS", "OCHOCIENTOS", "NOVECIENTOS"};
+    
+    String ConvertirMenoresOIgualesAQuince (int numero)
+    {
+        String resultado = "";
+        if (numero <= 15)
+        {
+            resultado = ArrCeroAQuince [numero];
+        }
+        return resultado;
+    }
+    
+    public boolean LeapYear (int year)
+    {
+        boolean result = false;
+        
+        if ((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0)))
+            result = true;
+        
+        return (result);
+    }
+    
+    String ConvertirNumerosEntreDieciseisYDiecinueve (int numero)
+    {
+        String resultado = "DIECI";
+        if (16 <= numero && numero <= 19)
+        {
+            resultado += ArrCeroAQuince [numero % 10];
+        }
+        return resultado;        
+    }
+    
+    String ConvertirNumerosEntreVeinteYNoventaYNueve (int numero)
+    {
+        String resultado = "";
+        if (20 <= numero && numero <= 99)
+        {
+            int cantidadDecenas = numero / 10;
+            String nombreDecena = ArrDiecis [cantidadDecenas];
+            int cantidadUnidad = numero % 10;
+            if (cantidadDecenas == 2)
+            {
+                // verifique si es 20
+                if (cantidadUnidad == 0)
+                    resultado = nombreDecena + "E";
+                else
+                {
+                    resultado = nombreDecena + "I" + ArrCeroAQuince [cantidadUnidad];
+                }
+            }
+            else
+            {
+                resultado = nombreDecena;
+                if (cantidadUnidad > 0)
+                    resultado += " Y " + ArrCeroAQuince [cantidadUnidad];
+            }
+        }
+        return resultado;        
+    }    
+    
+    public String ConvertirNumerosALetras (int numero)
+    {
+        String result = "";
+        if (numero <= 15)
+            result = ConvertirMenoresOIgualesAQuince (numero);
+        if (16 <= numero && numero <= 19)
+            result = ConvertirNumerosEntreDieciseisYDiecinueve(numero);
+        if (20 <= numero && numero <= 99)
+            result = ConvertirNumerosEntreVeinteYNoventaYNueve(numero);        
+        return (result);
+    }
+    
+    
     
     public String HelloWorldCustom (String name )
     {
@@ -20,6 +100,55 @@ public class EcuacionCuadratica {
             result = result + " " + name;
         }
         return (result);
+    }
+    
+    public int SumOfDigits (int number)
+    {
+        int result = 0;
+        
+        return result;
+    }
+    
+    public boolean MultipleOfThree (int number)
+    {
+        boolean result = false;
+        
+        
+        return result;
+    }
+    
+    public boolean PrimeNumber (int number)
+    {
+        boolean result = true;
+        int multiplo = 2;
+        
+        // pregunte si el número es par
+        if (number % multiplo == 0)
+            result = false;
+        else
+            // el número es impar.  proceda con la segunda optimización
+        {
+            multiplo = 3;
+            // verifique si es divisible entre 3
+            if (MultipleOfThree(number) == true)
+                result = false;
+            else
+            {
+                // itere hasta donde corresponda para buscar submúltiplos
+                int limite = number / 2;
+                while (result == true && multiplo <= limite)
+                {
+                    multiplo += 2;
+                    // verifique si el número es divisible entre el valor actual
+                    if (number % multiplo == 0 )
+                        result = false;
+                }
+            }
+            
+        }
+            
+        
+        return result;
     }
     
 }
--- Calentamiento/test/TestCuadratica.java
+++ Calentamiento/test/TestCuadratica.java
@@ -41,10 +41,213 @@ public class TestCuadratica {
     public void pruebaHelloWorldCustomCasoBase ()
     {
         String userName = "Alejandra";
-        String expectedMessage = "Hello World";
+        String expectedMessage = "Hello WORLD";
         EcuacionCuadratica ecu = new EcuacionCuadratica();
         // realice la ejecución de la prueba
         String realMessage = ecu.HelloWorldCustom(userName);
-        assertEquals(String.format("Error en la prueba"), expectedMessage, realMessage);        
+        assertEquals(String.format("Error en la prueba.  Se esperaba " +
+                "[%1s] y se obtuvo [%2s]", expectedMessage, realMessage), 
+                expectedMessage.toUpperCase(), realMessage.toUpperCase());        
     }
+    
+    @Test
+    public void pruebaHelloWorldBob ()
+    {
+        String userName = "Bob";
+        String expectedMessage = "Hello WORLD Bob";
+        EcuacionCuadratica ecu = new EcuacionCuadratica();
+        // realice la ejecución de la prueba
+        String realMessage = ecu.HelloWorldCustom(userName);
+        assertEquals(String.format("Error en la prueba.  Se esperaba " +
+                "[%1s] y se obtuvo [%2s]", expectedMessage, realMessage), 
+                expectedMessage.toUpperCase(), realMessage.toUpperCase());        
+    }
+
+    @Test
+    public void pruebaHelloWorldAlice ()
+    {
+        String userName = "Alice";
+        String expectedMessage = "Hello WORLD Alice";
+        EcuacionCuadratica ecu = new EcuacionCuadratica();
+        // realice la ejecución de la prueba
+        String realMessage = ecu.HelloWorldCustom(userName);
+        assertEquals(String.format("Error en la prueba.  Se esperaba " +
+                "[%1s] y se obtuvo [%2s]", expectedMessage, realMessage), 
+                expectedMessage.toUpperCase(), realMessage.toUpperCase());        
+    }
+
+@Test
+public void ConvertirNumerosALetrasCero ()
+{
+    int numeroAConvertir = 0;
+    String expectedString = "CERO";
+    String realString = "";
+    EcuacionCuadratica ecu = new EcuacionCuadratica();
+    // realice la ejecución de la prueba
+    realString = ecu.ConvertirNumerosALetras(numeroAConvertir);
+    assertEquals(String.format("Error en la prueba.  Se esperaba " +
+            "[%1s] y se obtuvo [%2s]", expectedString, realString), 
+            expectedString.toUpperCase(), realString.toUpperCase());        
+}
+   @Test
+public void ConvertirNumerosALetrasDiecis ()
+{
+    int numeroAConvertir = 17;
+    String expectedString = "DIECISIETE";
+    String realString = "";
+    EcuacionCuadratica ecu = new EcuacionCuadratica();
+    // realice la ejecución de la prueba
+    realString = ecu.ConvertirNumerosALetras(numeroAConvertir);
+    assertEquals(String.format("Error en la prueba.  Se esperaba " +
+            "[%1s] y se obtuvo [%2s]", expectedString, realString), 
+            expectedString.toUpperCase(), realString.toUpperCase());        
+} 
+
+   @Test
+public void ConvertirNumerosALetrasVeinte ()
+{
+    int numeroAConvertir = 20;
+    String expectedString = "VEINTE";
+    String realString = "";
+    EcuacionCuadratica ecu = new EcuacionCuadratica();
+    // realice la ejecución de la prueba
+    realString = ecu.ConvertirNumerosALetras(numeroAConvertir);
+    assertEquals(String.format("Error en la prueba.  Se esperaba " +
+            "[%1s] y se obtuvo [%2s]", expectedString, realString), 
+            expectedString.toUpperCase(), realString.toUpperCase());        
+} 
+
+   @Test
+public void ConvertirNumerosALetrasVeintis ()
+{
+    int numeroAConvertir = 21;
+    String expectedString = "VEINTIUNO";
+    String realString = "";
+    EcuacionCuadratica ecu = new EcuacionCuadratica();
+    // realice la ejecución de la prueba
+    realString = ecu.ConvertirNumerosALetras(numeroAConvertir);
+    assertEquals(String.format("Error en la prueba.  Se esperaba " +
+            "[%1s] y se obtuvo [%2s]", expectedString, realString), 
+            expectedString.toUpperCase(), realString.toUpperCase());        
+} 
+
+   @Test
+public void ConvertirNumerosALetrasDecenaExactaMayorAVeinte ()
+{
+    int numeroAConvertir = 50;
+    String expectedString = "CINCUENTA";
+    String realString = "";
+    EcuacionCuadratica ecu = new EcuacionCuadratica();
+    // realice la ejecución de la prueba
+    realString = ecu.ConvertirNumerosALetras(numeroAConvertir);
+    assertEquals(String.format("Error en la prueba.  Se esperaba " +
+            "[%1s] y se obtuvo [%2s]", expectedString, realString), 
+            expectedString.toUpperCase(), realString.toUpperCase());        
+} 
+
+   @Test
+public void ConvertirNumerosALetrasDecenaNoExactaMayorAVeinte ()
+{
+    int numeroAConvertir = 99;
+    String expectedString = "NOVENTA Y NUEVE";
+    String realString = "";
+    EcuacionCuadratica ecu = new EcuacionCuadratica();
+    // realice la ejecución de la prueba
+    realString = ecu.ConvertirNumerosALetras(numeroAConvertir);
+    assertEquals(String.format("Error en la prueba.  Se esperaba " +
+            "[%1s] y se obtuvo [%2s]", expectedString, realString), 
+            expectedString.toUpperCase(), realString.toUpperCase());        
+} 
+
+
+    @Test
+public void TestLeapYearOdd ()
+{
+    int myTestYear = 2013;
+    boolean expectedResult = false;
+    boolean realResult;
+    EcuacionCuadratica ecu = new EcuacionCuadratica();
+    // realice la ejecución de la prueba
+    realResult = ecu.LeapYear(myTestYear);
+    assertEquals(String.format("Error en la prueba.  Se esperaba " +
+            "[%1s] y se obtuvo [%2s]", expectedResult, realResult), 
+            expectedResult, realResult);        
+}
+
+@Test
+public void TestLeapYearEvenNotMultipleOfFour ()
+{
+    int myTestYear = 2014;
+    boolean expectedResult = false;
+    boolean realResult;
+    EcuacionCuadratica ecu = new EcuacionCuadratica();
+    // realice la ejecución de la prueba
+    realResult = ecu.LeapYear(myTestYear);
+    assertEquals (expectedResult, realResult);
+    /*
+    assertEquals(String.format("Error en la prueba.  Se esperaba " +
+            "%1s y se obtuvo %2s a las %3s, desde la IP %4s, por el usuario %5s", 
+            expectedResult, realResult, 
+            "01/01/2010", "127.0.0.1", "fernandezmh" ), 
+            expectedResult, realResult);        
+            */
+}
+
+@Test
+public void TestLeapYearEvenMultipleOfFourNotDividedFourHundred ()
+{
+    int myTestYear = 1700;
+    boolean expectedResult = false;
+    boolean realResult;
+    EcuacionCuadratica ecu = new EcuacionCuadratica();
+    // realice la ejecución de la prueba
+    realResult = ecu.LeapYear(myTestYear);
+    assertEquals(String.format("Error en la prueba.  Se esperaba " +
+            "[%1s] y se obtuvo [%2s]", expectedResult, realResult), 
+            expectedResult, realResult);        
+}
+
+
+
+@Test
+public void TestPrimeEvenNumber ()
+{
+    int myTestNumber = 2000000;
+    boolean expectedResult = false;
+    boolean realResult;
+    EcuacionCuadratica ecu = new EcuacionCuadratica();
+    // realice la ejecución de la prueba
+    realResult = ecu.PrimeNumber(myTestNumber);
+    assertEquals(String.format("Error en la prueba.  Se esperaba " +
+            "[%1s] y se obtuvo [%2s]", expectedResult, realResult), 
+            expectedResult, realResult);        
+}
+
+@Test
+public void TestPrimeNumberFive ()
+{
+    int myTestNumber = 5;
+    boolean expectedResult = true;
+    boolean realResult;
+    EcuacionCuadratica ecu = new EcuacionCuadratica();
+    // realice la ejecución de la prueba
+    realResult = ecu.PrimeNumber(myTestNumber);
+    assertEquals(String.format("Error en la prueba.  Se esperaba " +
+            "[%1s] y se obtuvo [%2s]", expectedResult, realResult), 
+            expectedResult, realResult);        
+}
+
+@Test
+public void TestNotPrimeHighNumber ()
+{
+    int myTestNumber = 10201; // 10201 = 101 * 101
+    boolean expectedResult = false;
+    boolean realResult;
+    EcuacionCuadratica ecu = new EcuacionCuadratica();
+    // realice la ejecución de la prueba
+    realResult = ecu.PrimeNumber(myTestNumber);
+    assertEquals(String.format("Error en la prueba.  Se esperaba " +
+            "[%1s] y se obtuvo [%2s]", expectedResult, realResult), 
+            expectedResult, realResult);        
+}
 }
