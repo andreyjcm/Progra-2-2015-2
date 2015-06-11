1858d1f12b2ec0a8a11225d0fa082dd9a5e3c031
laboratorio <laboratorio@Lab3-Profesor.ulatina.net>
2015-06-10 20:31:13 -0600

    se crean interfaces para mostrar herencia

---
+++ Calentamiento/src/Inheritance/ICircular.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Inheritance;
+
+/**
+ *
+ * @author laboratorio
+ */
+public interface ICircular extends IFiguraGeometrica {
+    double radio = 0.0;
+    public double diametro ();
+}
---
+++ Calentamiento/src/Inheritance/INoCirculares.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Inheritance;
+
+/**
+ *
+ * @author laboratorio
+ */
+public interface INoCirculares extends IFiguraGeometrica{
+     int cantidadLados  = 0;
+}
