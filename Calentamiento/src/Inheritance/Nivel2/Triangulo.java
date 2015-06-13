c5d633750b174aaaf41a1e53692cdd82a864407b
laboratorio <laboratorio@LAB05-PROFE.ulatina.net>
2015-06-12 19:59:21 -0600

    se implementa la clase Triángulo de la interfaz ITriangulo

--- Calentamiento/src/Inheritance/Nivel2/Triangulo.java
+++ Calentamiento/src/Inheritance/Nivel2/Triangulo.java
@@ -7,17 +7,61 @@ package Inheritance;
 
 
 public class Triangulo implements ITriangulo {
+    private double altura = 0.0;
+    
+        public void setAltura (double altura)
+        {
+            if (altura > 0.0)
+                this.altura = altura; 
+        }
+        
+    public double getAltura ()
+    {
+        return (altura);
+    }
+    
+    public double [] getLados ()
+    {
+        return MedidasLados;
+    }
+    public void getLados (double lado1, double lado2, double lado3)
+    {
+        lado1 = MedidasLados [0];
+        lado2 = MedidasLados [1];
+        lado3 = MedidasLados [2];
+    }
+
     
     private int cantidadLadosTriangulo = 3;
     private double[] MedidasLados;
     
-    public Triangulo (double lado1, double lado2, double lado3)
+    private void AsignarValoresAlTriangulo (double catetoMenor, double catetoMayor, 
+                                double hipotenusa)
     {
-        MedidasLados [0] = lado1;
-        MedidasLados [1] = lado2;
-        MedidasLados [2] = lado3;
+        if (catetoMenor + catetoMayor < hipotenusa)
+        {
+            MedidasLados [0] = catetoMenor;
+            MedidasLados [1] = catetoMayor;
+            MedidasLados [2] = hipotenusa;
+        }      
+        // ToDo:  caso de error cuando el triángulo no es válido
     }
     
+    public Triangulo (double lado)
+    {
+        AsignarValoresAlTriangulo (lado, lado, lado);
+    }
+    
+    public Triangulo (double catetoMenor, double catetoMayor, double hipotenusa)
+    {
+        AsignarValoresAlTriangulo (catetoMenor, catetoMayor, hipotenusa);
+    }
+     public Triangulo (double ladosIguales, double ladoDiferente)
+    {
+        AsignarValoresAlTriangulo (ladosIguales, ladosIguales, ladoDiferente);
+    }
+    
+   
     @Override
     public double[] ObtenerMedidas ()
     {
